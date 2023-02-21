557b263c1671c50638840c31ae317d015e4a9bf2
zahid-soft <zahidsarang95@gmail.com>
2023-02-21 17:58:54 +0530

    user register

--- services/JwtService.js
+++ services/JwtService.js
@@ -1,5 +1,11 @@
+import {JWT_SECRET} from '../config'
+import jwt from "jsonwebtoken";
+
 class JwtService {
-    static sign() {
-        
+    static sign(payload,expiry='60s',secret=JWT_SECRET) {
+        return jwt.sign(payload,secret,{expiresIn:expiry});
+
     }
 }
+
+export default JwtService;
